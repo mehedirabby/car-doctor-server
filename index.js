58a2548acaee7000fce7d2f6462380ee6e71dc34
Mehedi rabby <mehedi15-2995@diu.edu.bd>
2022-11-07 18:06:12 +0600

    token added

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
 // middlewares
